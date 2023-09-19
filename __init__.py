26a6317293f527703992a95a3b3256108a03b4f0
Alf Mikael Constantinou <amikaelc@protonmail.com>
2023-09-19 15:43:00 +0200

    added a bunch of stuff

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 import sys
+from pathlib import Path
 
-newlib = __file__[:-11] + "/lib"
+"""Any module that imports this module gets import access to the modules  
+in the 'lib' folder. 
+(For development it is suggested to add the lib folder to the pythonpath, to
+enable cde completion)
+"""
+cad_bundle_lib = Path(__file__).parent / Path("lib")
 
-if newlib not in sys.path:
-    sys.path.append(newlib)
+if cad_bundle_lib not in sys.path:
+    sys.path.append(str(cad_bundle_lib))
+
